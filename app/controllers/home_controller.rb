eed8531e8d8c1550f86a3a89b687145fa95b6b1c
jihyeChoi <cfgty12@naver.com>
2017-04-06 06:06:13 +0000

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-        @lotto = (1..45).to_a
-        @select_menu = @lotto.sample(6)
+        @lotto = (1..45).to_a #[1,2,3,4,5,.....,45]
+        @select_menu = @lotto.sample(6)#[1,2,5,6,45,32]
     end
 
 end
